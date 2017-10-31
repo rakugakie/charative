ed945866966e1462c469c66cb16d561aa125e120
rakugakie <rakugakie@hotmail.com>
2017-10-30 20:27:47 -0700

    'ss'

--- charativeproject/wsgi.py
+++ charativeproject/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "charative.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "charativeproject.settings.local")
 
 application = get_wsgi_application()
