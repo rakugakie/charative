171e62932264b9c47d6c4890cf6de4e33c5c45e7
rakugakie <rakugakie@hotmail.com>
2017-11-06 15:59:45 -0800

    first migrations

--- charative/models.py
+++ charative/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Modifiable(models.Model):
+    modified = models.DateField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class TimeCreatable(models.Model):
+    created = models.DateTimeField(auto_now_add=True, editable=False)
+
+
+class Chara(Modifiable, TimeCreatable):
+    name = models.CharField(unique =False, max_length=50)
+    picture = models.ImageField(upload_to='chara_images',blank=True)
---
+++ charativeproject/settings/production.py
@@ -0,0 +1,15 @@
+from .base import *  # noqa
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = [
+    'charative.herokuapp.com',
+]
+# Application definition
+
+# Database
+# https://docs.djangoproject.com/en/1.10/ref/settings/#databases
+
+DATABASES = {
+}
